12474bb05f8f8f50f158cd11dd18eb6d5484a985
liboru <1036513264@qq.com>
2021-07-22 11:50:38 +0800

    case01-v8：getCharge/getFrequentRenterPoints 逻辑由 Rental 转移到 Movie

--- src/main/java/com/liboru/reconsitution/learn/case01/Movie.java
+++ src/main/java/com/liboru/reconsitution/learn/case01/Movie.java
@@ -17,4 +17,34 @@ public class Movie {
     @Setter
     private int priceCode;
 
+    public double getCharge(int daysRented){
+        double result = 0;
+        switch (this.priceCode) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((this.priceCode == Movie.NEW_RELEASE) &&
+                daysRented > 1) {
+            return 2;
+        }
+        return 1;
+    }
+
 }
